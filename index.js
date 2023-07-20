5273fd005bf866d832419dcc2a828e46aca03da8
Kweku Gideon <50534877+KwekuGideon7@users.noreply.github.com>
2023-07-20 19:28:11 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// const list = document.querySelector('#hidden');
+// const menu = document.querySelector('#menu');
+
+// menu.addEventListener('click', () => {
+//    if(list.classList.contains('hide')){
+//          list.classList.remove('hide');
+//         //  list.classList.add('show');
+//    }
+//     });
+
+
+
+// const menu = document.getElementById('menu');
+// const ulElements = document.querySelectorAll('ul');
+// let isHidden = false;
+
+// menu.addEventListener('click', () => {
+//     if (isHidden) {
+//         ulElements.forEach(ulElement => {
+//             ulElement.style.display = 'flex';
+//         });
+//     } else {
+//         ulElements.forEach(ulElement => {
+//             ulElement.style.display = 'none';
+//         });
+//     }
+//     isHidden = !isHidden; // Toggle the state
+// });
